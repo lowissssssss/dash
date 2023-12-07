78af6370c8d402a5df50f534ee30845045b8a378
raishudesu <bacaltosbaryshnikov@gmail.com>
2023-12-07 15:02:01 +0800

    added cart

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ const editProductForm = document.getElementById("editProductForm");
 const addProductForm = document.getElementById("addProductForm");
 const productsContainer = document.getElementById("productsContainer");
 
+const cartContainer = document.getElementById("cartContainer");
+const totalSale = document.getElementById("totalSale");
+
 let productName = "";
 let productDescription = "";
 let productPrice = "";
@@ -109,7 +112,7 @@ const productCard = (name, desc, price, link, index) => {
                             Edit
                         </button>
                     </div>
-                    <button type="button" class="btn btn-outline-success">
+                    <button type="button" class="btn btn-outline-success" onclick="addToCart(${index})">
                         Add to cart
                     </button>
               </div>
@@ -190,7 +193,8 @@ editProductImageLinkInput.addEventListener("input", () => {
   editProductImageLink = editProductImageLinkInput.value;
 });
 
-editProductForm.addEventListener("submit", () => {
+editProductForm.addEventListener("submit", (e) => {
+  // e.preventDefault();
   const currentProducts = JSON.parse(localStorage.getItem("products"));
 
   // Check if the index is valid
@@ -211,6 +215,7 @@ editProductForm.addEventListener("submit", () => {
 
     // Inform the user about the successful update
     alert("Product updated successfully!");
+    // showProducts();
   } else {
     console.log("Invalid index for updating product.");
   }
@@ -238,5 +243,143 @@ const deleteProduct = (index) => {
     } else {
       console.log("Invalid index for deleting product.");
     }
+  }
+};
+
+// ADD TO CART
+const addToCart = (index) => {
+  const products = JSON.parse(localStorage.getItem("products")) || [];
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  const productToAdd = products[index];
+
+  if (!productToAdd) {
+    return;
+  }
+
+  const existingProductIndex = cart.findIndex(
+    (product) => product.index === index
+  );
+
+  if (existingProductIndex !== -1) {
+    cart[existingProductIndex].quantity++;
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert("Added to cart!");
+  } else {
+    const newProduct = {
+      ...productToAdd,
+      index,
+      quantity: 1,
+    };
+
+    localStorage.setItem("cart", JSON.stringify([...cart, newProduct]));
+    alert("Added to cart!");
+  }
+  showCurrentCart();
+};
+
+// VIEW CART
+
+const productCardCart = (name, desc, price, link, quantity, index) => {
+  return `
+    <tr>
+      <td class="w-25">
+        <img
+          src="${link}"
+          class="img-fluid img-thumbnail"
+          alt="${link}"
+        />
+      </td>
+      <td>${name}</td>
+      <td>₱${price}</td>
+      <td class="qty">
+        <input
+          type="number"
+          class="form-control"
+          id="inputQuantity"
+          value=${quantity}
+          onchange="updateQuantity(${index}, this.value)"
+        />
+      </td>
+      <td>₱${quantity * Number(price)}</td>
+      <td>
+        <button class="btn btn-danger btn-sm" onclick="removeProductFromCart(${index})">
+         Remove
+        </button>
+      </td>
+    </tr>
+  `;
+};
+
+const calculateTotalPrice = () => {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  let totalPrice = 0;
+
+  cart.forEach((cartItem) => {
+    totalPrice += Number(cartItem.productPrice) * cartItem.quantity;
+  });
+
+  return totalPrice;
+};
+
+const showCurrentCart = () => {
+  const currentCart = JSON.parse(localStorage.getItem("cart"));
+
+  if (!currentCart || currentCart.length === 0) {
+    cartContainer.innerHTML = "Cart empty.";
+    return;
+  }
+  const mappedProducts = currentCart.map(
+    (
+      {
+        productName,
+        productDescription,
+        productPrice,
+        productImageLink,
+        quantity,
+      },
+      index
+    ) =>
+      productCardCart(
+        productName,
+        productDescription,
+        productPrice,
+        productImageLink,
+        quantity,
+        index
+      )
+  );
+
+  cartContainer.innerHTML = mappedProducts.join("");
+  totalSale.textContent = `₱${calculateTotalPrice()}`;
+};
+
+showCurrentCart();
+
+const removeProductFromCart = (index) => {
+  const cart = JSON.parse(localStorage.getItem("cart"));
+
+  if (index >= 0 && index < cart.length) {
+    cart.splice(index, 1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert("Product deleted from cart successfully!");
+    showCurrentCart();
+  } else {
+    console.log("Invalid index for deleting product from cart.");
+  }
+};
+
+const updateQuantity = (index, newQuantity) => {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  if (index >= 0 && index < cart.length) {
+    cart[index].quantity = parseInt(newQuantity);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    totalSale.textContent = `₱${calculateTotalPrice()}`;
+    showCurrentCart();
+    // Update the display or perform other actions as needed
+  } else {
+    console.log("Invalid index for updating quantity in cart.");
   }
 };
